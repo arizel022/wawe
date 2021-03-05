abc6dc599e16b0a5c7c07fa15f18d348d3bd00dc
arizel022 <arizel022@gmail.com>
2021-03-05 15:53:54 +0100

    fixed

--- app/js/main.js
+++ app/js/main.js
@@ -5,9 +5,38 @@ $(document).ready(function() {
         // autoplay: true,
         arrows: false
     });
-    $(".menu__burger").click(function(){
+    
+    $('.menu__burger').click(function(){
 		$('.menu__list').slideToggle();
 	});
+    $('.menu__burger').click(function () {
+        $('.menu__burger').toggleClass('active');
+    });
+
+    $('.hero').on('click','a', function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+
+    var mixer = mixitup('.gallery__pictures');
+
+    $('.fancybox').fancybox({
+        padding : 30,
+        helpers:  {
+            overlay : {
+                css : {
+                    'background' : 'rgba(0, 0, 0, 0.5)'
+                }
+            }
+        }
+    });
+
+    $('.video__play').click(function(){
+		$('.fancybox').click();
+	});
+
 });
 
 
